07a2216dba7c2237bbc94d2a4a85663152541a09
unknown <alkham007@example.com>
2023-03-23 19:02:07 +0300

    project preparation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src="https://img2.freepng.ru/20180629/xtu/kisspng-vk-social-networking-service-yandex-search-like-bu-russia-player-5b36bc6059a9d8.2401446715303138243673.jpg" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import s from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div>
+                Profile
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
