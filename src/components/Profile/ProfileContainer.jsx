bd349cfd960f8a183bbb473378981bc43c8d8f4b
unknown <alkham007@mail.ru>
2023-05-02 17:45:14 +0300

    add withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,19 +3,24 @@ import React from "react";
 import { connect } from "react-redux";
 import Profile from "./Profile";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useLocation, useNavigate, useParams } from "react-router";
 
 class ProfileAPIContainer extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-        .then(response => {
-            this.props.setUserProfile(response.data);
-        })
+        let userId = this.props.router.params.userId;
+        if (!userId) {
+            userId = 2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+            .then(response => {
+                this.props.setUserProfile(response.data);
+            })
     }
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile}/> //прокидываем все пропсы дальше
+            <Profile {...this.props} profile={this.props.profile} /> //прокидываем все пропсы дальше
         )
     }
 }
@@ -24,7 +29,26 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
+// ProfileAPIContainer - классовая компонента, мы не можем использовать хуки в классвовых компонентах. 
+// Есть решение из оффициальной документации - создать функцию-обёртку, которая по принципу идентична к withRouter
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
 
-const ProfileContainer = connect(mapStateToProps, {setUserProfile})(ProfileAPIContainer)
+    return ComponentWithRouterProp;
+}
+
+let WithUrlDataContainerComponent = withRouter(ProfileAPIContainer);
+
+const ProfileContainer = connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent)
 
 export default ProfileContainer;
