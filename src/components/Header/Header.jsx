befef3795cb651454a8ee125f92b466ddeae67cd
unknown <alkham007@mail.ru>
2023-05-06 10:37:46 +0300

    auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src='https://static.tildacdn.com/tild6138-3535-4466-a135-393166633639/photo-2-1536x1536.png' />
+            <div className={s.loginBlock}>
+                { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+            </div>
         </header>
     )
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,14 +3,17 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({   // по сути это наш state, мы говорим что за profilePage отвечает profileReducer и т.д.
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers); // createStore создает внутри себя state у которого есть три свойства
+window.store = store;
 
 export default store;
