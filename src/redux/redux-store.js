54b26ef6fe65d7848c53614f1c741cf5352e21cd
Yusyf <alkham007@mail.ru>
2023-06-29 18:01:32 +0300

    login

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 
 let reducers = combineReducers({   // по сути это наш state, мы говорим что за profilePage отвечает profileReducer и т.д.
@@ -12,7 +13,8 @@ let reducers = combineReducers({   // по сути это наш state, мы г
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware)); // createStore создает внутри себя state у которого есть * свойства
